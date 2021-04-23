903c3be3e38f6d1d254854c303ab7963b9522687
Xymaru <xyma.ru99@gmail.com>
2021-04-23 18:24:54 +0200

    Whole project swap
    Swapped our custom base project with the template given by the teachers.

--- MegaBomberman/MegaBomberman/src/headers/math/Sizef.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-struct Sizef {
-	float width, height;
-};
--- MegaBomberman/MegaBomberman/src/headers/math/Vector2f.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct Vector2f {
-	float x, y;
-};
-
-inline void operator*=(Vector2f& origin, float value) { origin.x *= value; origin.y *= value; }
-inline void operator-=(Vector2f& origin, float value) { origin.x -= value; origin.y -= value; }
